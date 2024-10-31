c50a80b1b0e90384d3bf11015bd9edb8ed56d8fd
Yasin YILDIZ <144591826+yasinfedev@users.noreply.github.com>
2024-10-31 15:41:15 +0300

    Create degiskenler.swift

---
+++ degiskenler.swift
@@ -0,0 +1,43 @@
+var urun_id : Int = 3416
+var urun_adi : String = "Kol saati"
+var urun_adet : Int = 100
+var urun_fiyat : Double = 149.99
+var urun_tedarikci : String = "rolex"
+
+print(urun_id)
+print(urun_adi)
+print(urun_adet)
+print(urun_fiyat)
+print(urun_tedarikci)
+print("Ürünün id kodu: \(urun_id)'dır.")
+
+var ogrenci_adi : String = "Yasin"
+print(ogrenci_adi)
+
+print(1.0, 2.0, 3.0)
+print("Yasin", "Mine", "Zeynep", separator: "-")
+
+// çıktıdaki string ifade içine değişken eklemek: \(değişken) ile.
+var ad = "Yasin"
+var yil = 2
+
+print("\(ad) Konya'da \(yil) yıldır yaşamaktadır.")
+
+var sayi1 = 10, sayi2 = 20, isim1 = "Mine", kesir = 2.3
+print(sayi1, sayi2, isim1, kesir)
+
+var numara : Int? // Optional
+numara = 26
+print(numara!)
+
+var toplam = yil + urun_id
+print(toplam)
+
+var sayi4 = 45
+print(sayi4)
+sayi4 = 47
+print(sayi4)
+
+
+
+
